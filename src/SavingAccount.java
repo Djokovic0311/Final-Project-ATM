fdb048ff2e22e57eacdad660ed2fda498c061b3d
Djokovic0311 <55948986+Djokovic0311@users.noreply.github.com>
2022-12-04 21:43:32 -0500

    add saving account

--- src/SavingAccount.java
+++ src/SavingAccount.java
@@ -1,3 +1,4 @@
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
 
@@ -7,6 +8,8 @@ import java.util.Map;
     @version: 1.0
 */
 public class SavingAccount extends Account{
+    // interest rate, by default 0.01
+    private final double interestRate = 0.01;
     // interest in this saving account, which should be redeemed manually by customer
     private double interest;
     // lastest redeem record
@@ -34,6 +37,35 @@ public class SavingAccount extends Account{
         else return false;
     }
 
+    // gain interest by month
+    public void gainInterest() {
+        Date now = new Date();
+        Calendar c1 = Calendar.getInstance();
+        Calendar c2 = Calendar.getInstance();
+        c1.setTime(lastRedeemDate);
+        c2.setTime(now);
+        int year1 = c1.get(Calendar.YEAR);
+        int year2 = c2.get(Calendar.YEAR);
+        int month1 = c1.get(Calendar.MONTH);
+        int month2 = c2.get(Calendar.MONTH);
+        int day1 = c1.get(Calendar.DAY_OF_MONTH);
+        int day2 = c2.get(Calendar.DAY_OF_MONTH);
+        int yearInterval = year1 - year2;
+        if (month1 < month2 || month1 == month2 && day1 < day2) {
+            yearInterval--;
+        }
+        int monthInterval = (month1 + 12) - month2;
+        if (day1 < day2) {
+            monthInterval--;
+        }
+        monthInterval %= 12;
+        int monthsDiff = Math.abs(yearInterval * 12 + monthInterval);
+        this.interest += monthsDiff * interestRate;
+    }
+
+    public void gainInterest(Date date) {
+
+    }
     //getters and setters
 
     public double getInterest() {
