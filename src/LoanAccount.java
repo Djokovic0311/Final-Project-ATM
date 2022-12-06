d850b7fca2611dfc2f4ba06a72cd84f5e98aa706
Doppelgangermatryoshka <52772832+Doppelgangermatryoshka@users.noreply.github.com>
2022-12-06 16:22:38 -0500

    Add files via upload

---
+++ src/LoanAccount.java
@@ -0,0 +1,28 @@
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class LoanAccount extends Account{
+
+    public LoanAccount(int accountID, AccountType type, String ownerName, String pwd)
+    {
+    	super(accountID, type, ownerName, pwd);
+    	
+    }
+
+    public LoanAccount(int accountID, String ownerName, String pwd, AccountType type, Map<Currency, Double> balance) {
+        this(accountID, ownerName, pwd, type);
+        setBalance(balance);
+    }
+    
+    public static String createLoanAccount(AccountType type, Customer customer, int startBalance){
+        if (customer.getSavingAccounts()!=null){
+            customer.getSavingAccount(super.accountID).withdraw(super.accountID, startBalance);//function not exist yet
+            customer.createAccount(type, startBalance);}
+    }
+
+    public Loan[] checkLoans() {
+    	return //read from SQL
+    }
+}
+
