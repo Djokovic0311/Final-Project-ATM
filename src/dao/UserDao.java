7e5bca3dab0fbdfe7ea07f6b8af9e0dccefa9725
sanathbhimsen <sanathbhimsen26@gmail.com>
2022-12-13 15:08:28 -0500

    Added the User Dao

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -2,18 +2,37 @@ package dao;
 
 import model.User;
 
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class UserDao {
 
 
     // select user by id
-    public User selectUserById(int id) {
+    public User selectUserById(int id) throws SQLException {
         // TODO:
-        return null;
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Bank", "root", "108875556");
+        Statement stmt = con.createStatement();
+        User user;
+        ResultSet rs;
+        rs = stmt.executeQuery("SELECT * FROM Person WHERE ID =" + id + ";");
+        while(rs.next()){
+            user = new User(rs.getString(1), rs.getInt(0),rs.getInt(2),rs.getInt(3));
+
+        }
+        return user;
     }
-    public boolean insertIntoUser(int id, String name, String pwd) {
+    public boolean insertIntoUser(int id, String name, String pwd) throws SQLException {
         // TODO:
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Bank", "root", "108875556");
+        Statement stmt = con.createStatement();
+        ResultSet rs;
+        try {
+            stmt.executeQuery("INSERT INTO Person (ID, userName, userPassword, type)" +
+                    "VALUES ( " + id + ", " + name + ", " + pwd + ", " + "customer" +  ");");
+        }catch(Exception E){
+            return false;
+        }
+
         return true;
     }
 
