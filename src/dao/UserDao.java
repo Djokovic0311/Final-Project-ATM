e57309625e5d6dba14759f728de31f05c199e903
sanathbhimsen <sanathbhimsen26@gmail.com>
2022-12-13 17:00:41 -0500

    Updated the latest user Dao

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -8,18 +8,18 @@ public class UserDao {
 
 
     // select user by id
-    public User selectUserById(int id) throws SQLException {
+    public User selectUserById(int id, String type) throws SQLException {
         // TODO:
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Bank", "root", "108875556");
         Statement stmt = con.createStatement();
         User user;
         ResultSet rs;
-        rs = stmt.executeQuery("SELECT * FROM Person WHERE ID =" + id + ";");
+        rs = stmt.executeQuery("SELECT * FROM Person WHERE ID =" + id + "AND type=" + type + ";");
         while(rs.next()){
-            user = new User(rs.getString(1), rs.getInt(0),rs.getInt(2),rs.getInt(3));
-
+            user = new User(rs.getString(1), rs.getInt(0),rs.getString(2),rs.getString(3));
+            return user;
         }
-        return user;
+        return null;
     }
     public boolean insertIntoUser(int id, String name, String pwd) throws SQLException {
         // TODO:
@@ -27,7 +27,7 @@ public class UserDao {
         Statement stmt = con.createStatement();
         ResultSet rs;
         try {
-            stmt.executeQuery("INSERT INTO Person (ID, userName, userPassword, type)" +
+            stmt.executeQuery("INSERT INTO Person (ID, userName, userPassword)" +
                     "VALUES ( " + id + ", " + name + ", " + pwd + ", " + "customer" +  ");");
         }catch(Exception E){
             return false;
