1f45e054f695e5181071dc9d5ab7998ab5c2c357
czhuai <61261586+czhuai@users.noreply.github.com>
2022-12-14 13:02:06 -0500

    Update StockDao

--- src/dao/StockDao.java
+++ src/dao/StockDao.java
@@ -1,17 +1,68 @@
 package dao;
 
+import java.sql.*;
+
+
 public class StockDao {
     public double getPriceByID(int stockID) {
-        return 0.0;
+        try {
+            String query = "SELECT * FROM StockMarket where stockID = ?;";
+            Connection conn = ConnectDao.connectToDb();
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setInt(1, stockID);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                double price = rs.getDouble(1);
+                return price;
+            }
+            return 0.0;
+        } catch (Exception e) { return 0.0; }
     }
+
     // check stock existence first, if it exists, update stock price and return true;
     //else return false;
-    public boolean updatePriceByID(int stockID, double price){
-        return true;
+    public boolean updatePriceByID(int stockID, double price) {
+        try {
+            String query = "SELECT * FROM StockMarket where stockID = ?;";
+            Connection conn = ConnectDao.connectToDb();
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setInt(1, stockID);
+            ResultSet rs = stmt.executeQuery();
+            if (!rs.next()) {
+                return false;
+            } else {
+                //stock price will be updated
+                query = "UPDATE StockMarket SET price = ? Where stockID= ?;";
+                stmt = conn.prepareStatement(query);
+                stmt.setDouble(1, price);
+                stmt.setInt(2, stockID);
+                stmt.executeQuery();
+                return true;
+            }
+        } catch (Exception e) { return false; }
+
     }
-    // check stock existence first, if it doesnt, insert stock  and return true;
+
+    // check stock existence first, if it doesn't, insert stock  and return true;
     // else return false;
-    public boolean insertIntoStock(int stockID, double price){
-        return true;
+    public boolean insertIntoStock(int stockID, double price) {
+        try {
+            String query = "SELECT * FROM StockMarket where stockID = ?;";
+            Connection conn = ConnectDao.connectToDb();
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setInt(1, stockID);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) { // Already exist
+                return false;
+            } else {
+                query = "INSERT INTO StockMarket (stockID, price) VALUES (?,?);";
+                stmt = conn.prepareStatement(query);
+                stmt.setInt(1, stockID);
+                stmt.setDouble(2, price);
+                stmt.executeUpdate();
+                return true;
+            }
+        } catch (Exception e) { return false; }
+
     }
 }
