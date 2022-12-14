c1ade47644ab1c0c8f3460edd1b4d8d5bdff265c
sanathbhimsen <sanathbhimsen26@gmail.com>
2022-12-14 02:55:23 -0500

    Added StockDao

--- src/dao/StockDao.java
+++ src/dao/StockDao.java
@@ -1,14 +1,61 @@
+
+
 package dao;
 
-import model.marketStock;
-
-import java.util.ArrayList;
+import java.sql.*;
 
 public class StockDao {
-    public double getPriceByID(int stockID) {
+    public double getPriceByID(int stockID) throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Bank", "root", "108875556");
+        Statement stmt = con.createStatement();
+        ResultSet rs;
+        rs = stmt.executeQuery("SELECT * FROM StockMarket where stockID = " + stockID + ";");
+        while(rs.next()){
+            double price = rs.getDouble(1);
+            return price;
+        }
         return 0.0;
     }
-    public ArrayList<marketStock> getMarketStocks(){
-        return null;
+    // check stock existence first, if it exists, update stock price and return true;
+    //else return false;
+    public boolean updatePriceByID(int stockID, double price) throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Bank", "root", "108875556");
+        Statement stmt = con.createStatement();
+        ResultSet rs;
+        rs = stmt.executeQuery("SELECT * FROM StockMarket where stockID = " + stockID + ";");
+
+        int count = 0;
+        while(rs.next()){
+            count = count + 1;
+        }
+
+        if(count==0){
+            return false;
+        }else{
+            //stock price will be updated
+            rs = stmt.executeQuery("UPDATE StockMarket SET price =" + price + "Where stockID= stockID" + ";");
+            return true;
+        }
+
+    }
+    // check stock existence first, if it doesnt, insert stock  and return true;
+    // else return false;
+    public boolean insertIntoStock(int stockID, double price) throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Bank", "root", "108875556");
+        Statement stmt = con.createStatement();
+        ResultSet rs;
+        rs = stmt.executeQuery("SELECT * FROM StockMarket where stockID = " + stockID + ";");
+        int count = 0;
+        while(rs.next()){
+            count = count + 1;
+        }
+        if(count!=0){
+            return false;
+        }else{
+            stmt.executeQuery("INSERT INTO StockMarket (stockID, price)" +
+                    "VALUES ( " + stockID + ", " + price +  ");");
+            return true;
+        }
+        
     }
 }
